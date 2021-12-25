c7985f2e7e4c57167733e595d1cfb25a74655f69
Shivam Rane <shivamprane@gmail.com>
2021-12-25 20:36:42 +0530

    Create single_inheritance.cpp

---
+++ single_inheritance.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class vehicle
+{
+protected:
+    string color;
+public:
+    vehicle();
+    ~vehicle();
+};
+
+vehicle::vehicle()
+{
+    cout << "vehicle" << endl;
+    color = "RED";
+    cout << "Color is " << color << endl;
+}
+
+vehicle::~vehicle()
+{
+}
+
+class car: public vehicle{                           // single inhertance
+    string model;
+    public:
+    car();
+    ~car();
+
+};
+
+car::car()
+{
+    cout << "Car" << endl;
+}
+car::~car(){}
+
+int main()
+{
+    
+    //black black1;
+    car car1;
+
+
+
+}
